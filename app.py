e13781dd56b652d63bb8c858a22ef3cdd6016d32
Chris Harris <chris@denimandsteel.com>
2023-01-23 11:17:25 -0800

    Initial commit

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import connexion
 from connexion import NoContent
 import swagger_ui_bundle
 
+import mysql.connector 
+import pymysql
+import yaml
+import logging
+import logging.config
+
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,7 +18,11 @@ from base import Base
 from buy import Buy
 from sell import Sell
 
-DB_ENGINE = create_engine("sqlite:///events.sqlite")
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+# TODO: create connection string, replacing placeholders below with variables defined in log_conf.yml
+DB_ENGINE = create_engine(f"mysql+pymysql://user:password@hostname:port/db")
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
@@ -20,10 +30,13 @@ DB_SESSION = sessionmaker(bind=DB_ENGINE)
 def buy(body):
     # TODO create a session
 
-    # TODO create a Buy object and populate it with values from the body
+    # TODO additionally pass trace_id (along with properties from Lab 2) into Buy constructor
 
     # TODO add, commit, and close the session
 
+    # TODO: call logger.debug and pass in message "Stored buy event with trace id <trace_id>"
+
+    # TODO return NoContent, 201
     return NoContent, 201
 # end
 
@@ -34,9 +47,11 @@ def get_buys():
 def sell(body):
     # TODO create a session
 
-    # TODO create a Buy object and populate it with values from the body
+    # TODO additionally pass trace_id (along with properties from Lab 2) into Sell constructor
 
     # TODO add, commit, and close the session
+
+    # TODO: call logger.debug and pass in message "Stored buy event with trace id <trace_id>"
 
     return NoContent, 201
 # end
@@ -47,6 +62,12 @@ def get_sells():
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basic')
 
 if __name__ == "__main__":
     app.run(port=8090)
