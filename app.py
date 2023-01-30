cd2c33f8e65e7d10389340e9966cfa1fcbabc972
Chris Harris <christopherharris@fastmail.fm>
2023-01-30 10:51:10 -0800

    Initial commit

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ import connexion
 from connexion import NoContent
 import swagger_ui_bundle
 
-import mysql.connector 
-import pymysql
-import yaml
-import logging
-import logging.config
-
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -18,11 +12,7 @@ from base import Base
 from buy import Buy
 from sell import Sell
 
-with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
-
-# TODO: create connection string, replacing placeholders below with variables defined in log_conf.yml
-DB_ENGINE = create_engine(f"mysql+pymysql://user:password@hostname:port/db")
+DB_ENGINE = create_engine("sqlite:///events.sqlite")
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
@@ -30,44 +20,57 @@ DB_SESSION = sessionmaker(bind=DB_ENGINE)
 def buy(body):
     # TODO create a session
 
-    # TODO additionally pass trace_id (along with properties from Lab 2) into Buy constructor
+    # TODO create a Buy object and populate it with values from the body
 
     # TODO add, commit, and close the session
 
-    # TODO: call logger.debug and pass in message "Stored buy event with trace id <trace_id>"
-
-    # TODO return NoContent, 201
     return NoContent, 201
 # end
 
-def get_buys():
-    # placeholder for future labs
-    pass
+def get_buys(timestamp):
+    # TODO create a DB SESSION
+
+    # TODO query the session and filter by Buy.date_created >= timestamp
+    # e.g. rows = session.query(Buy).filter etc...
+
+    # TODO create a list to hold dictionary representations of the rows
+    # e.g. data = []
+    # TODO loop through the rows, appending each row (use .to_dict() on each row) to 'data'
+
+    # TODO close the session
+
+    # TODO log the request to get_buys including the timestamp and number of results returned
+
+    return data, 200
 
 def sell(body):
     # TODO create a session
 
-    # TODO additionally pass trace_id (along with properties from Lab 2) into Sell constructor
+    # TODO create a Buy object and populate it with values from the body
 
     # TODO add, commit, and close the session
-
-    # TODO: call logger.debug and pass in message "Stored buy event with trace id <trace_id>"
 
     return NoContent, 201
 # end
 
-def get_sells():
-    # placeholder for future labs
-    pass
+def get_sells(timestamp):
+    # TODO create a DB SESSION
+
+    # TODO query the session and filter by Sell.date_created >= timestamp
+    # e.g. rows = session.query(Sell).filter etc...
+
+    # TODO create a list to hold dictionary representations of the rows
+    # e.g. data = []
+    # TODO loop through the rows, appending each row (use .to_dict() on each row) to 'data'
+
+    # TODO close the session
+
+    # TODO log the request to get_sells including the timestamp and number of results returned
+
+    return data, 200
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
-
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-logger = logging.getLogger('basic')
 
 if __name__ == "__main__":
     app.run(port=8090)
