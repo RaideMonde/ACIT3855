aa30e63dd4be4c12d80c596ae8710a4bdd2b4776
Rahul Singh <i.am.rahul@live.com>
2023-02-02 13:50:28 -0800

    added (given) processing and replaced old storage with new storage

--- old/Storage/app.py
+++ old/Storage/app.py
@@ -5,6 +5,12 @@ import connexion
 from connexion import NoContent
 import swagger_ui_bundle
 
+import mysql.connector 
+import pymysql
+import yaml
+import logging
+import logging.config
+
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,46 +18,79 @@ from base import Base
 from buy import Buy
 from sell import Sell
 
-DB_ENGINE = create_engine("sqlite:///events.sqlite")
+with open('./Storage/app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+# TODO: create connection string, replacing placeholders below with variables defined in log_conf.yml
+user     = app_config['user']
+password = app_config['password']
+hostname = app_config['hostname']
+port     = app_config['port']
+db       = app_config['db']
+DB_ENGINE = create_engine(f"mysql+pymysql://{user}:{password}@{hostname}:{port}/{db}")
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
 # Endpoints
 def buy(body):
-    
+    # TODO create a session
     session = DB_SESSION()
 
-    buy = Buy(
-        body['buy_id'],
-        body['item_name'],
+    # TODO additionally pass trace_id (along with properties from Lab 2) into Buy constructor
+    b = Buy(
+        body ['buy_id'],
+        body['trace_id'],
         body['item_price'],
-        body['buy_qty']
+        body['buy_qty'],
+        body['trace_id']
     )
-    
-    session.add(buy)
+    # TODO add, commit, and close the session
+    session.add(b)
     session.commit()
     session.close()
-    
-    return NoContent, 201
 
-def get_buys():
+    # TODO: call logger.debug and pass in message "Stored buy event with trace id <trace_id>"
+    logger.debug(f"Stored buy event with {body['trace_id']}")
+    # TODO return NoContent, 201
+    return NoContent, 201
+# end
+
+def get_buys(timestamp):
     # placeholder for future labs
+    session = DB_SESSION()
+
+    # TODO query the session and filter by Buy.date_created >= timestamp
+    # e.g. rows = session.query(Buy).filter etc
+    rows = session.query(Buy).filter(Buy.date_created >= timestamp)
+
+    # TODO create a list to hold dictionary representations of the rows
+    data = []
+    #TODO loop through the rows, appending each row( use.to_dict() on each row) to 'data'
+    row = row.to_dict()
+
+    #TODO close the session
+    #TODO log the request ot get_buys inccluding the timestamp of the number of results returned
     pass
 
 def sell(body):
     session = DB_SESSION()
-    sell = Buy(
+
+    s = Sell(
         body['sell_id'],
-        body['item_name'],
+        body['trace_id'],
         body['item_price'],
-        body['sell_qty']
+        body['sell_qty'],
+        body['trace_id']
     )
-    session.add(sell)
+    # TODO add, commit, and close the session
+    session.add(s)
     session.commit()
     session.close()
+
+    logger.debug(f"Stored sell event with {s['trace_id']}")
     
     return NoContent, 201
-# end
+
 
 def get_sells():
     # placeholder for future labs
@@ -59,6 +98,12 @@ def get_sells():
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
+
+with open('./Storage/log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basic')
 
 if __name__ == "__main__":
     app.run(port=8090)
--- old/Storage/sell.py
+++ old/Storage/sell.py
@@ -3,30 +3,38 @@ from base import Base
 import datetime
 
 class Sell(Base):
+    # TODO declare table name
     __tablename__ = "sell"
 
     id = Column(Integer, primary_key=True)
-    sell_id = Column(String(250), nullable=False)
-    item_name = Column(String(250), nullable = False)
-    item_price = Column(Integer, nullable=False)
-    sell_qty = Column(Integer, nullable=False)
-    date_created = Column(DateTime, nullable=False)
     
+    # TODO create the necessary columns following the id example above
+    sell_id = Column(String(250), nullable=False)
+    item_name = Column(String(250), nullable=False)
+    item_price = Column(Float, nullable=False)
+    sell_qty = Column(Integer, nullable=False)
+    trace_id = Column(String(250), nullable=False)
+    date_created = Column(String(100), nullable=False)
 
-    def __init__(self, sell_id, item_name, item_price, sell_qty):
-        
+    def __init__(self, sell_id, item_name, item_price, sell_qty, trace_id):
+        # TODO assign the parameter values to the object's properties
         self.sell_id = sell_id
         self.item_name = item_name
         self.item_price = item_price
         self.sell_qty = sell_qty
-        self.date_created = datetime.datetime.now()
+        self.trace_id = trace_id
+        self.date_created = datetime.datetime.now() # Sets the date/time record is created
 
     def to_dict(self):
+        # TODO create a dict, and assign object properties to your dict
         dict = {}
+        dict['id'] = self.id
         dict['sell_id'] = self.sell_id
         dict['item_name'] = self.item_name
         dict['item_price'] = self.item_price
         dict['sell_qty'] = self.sell_qty
+        dict['trace_id'] = self.trace_id
         dict['date_created'] = self.date_created
 
+        # TODO return dict
         return dict
